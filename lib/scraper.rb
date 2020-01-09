523cdb2ddedc72eb555e639be3ada1f540bf856a
Daniel Reina <dannyflatiron@gmail.com>
2020-01-09 03:57:31 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,32 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    # what does this do
+    # pulling data via scraping from a web page
+    # return value should be an array of hashes each hash represents one student
+    # how?
+    web_page = Nokogiri::HTML(open(index_url))
+    list = []
+    students = {}
+
+    web_page.css("div.student-card").each do |student|
+
+      students =
+      {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute('href').value
+      }
+      list << students
+    end
+    list
+    # binding.pry
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    # what does this do?
+    # uses data from scrape_index_page to add attributes to new instances of Students
   end
 
 end
-
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -21,7 +21,7 @@ describe "Scraper" do
  :bio=>
   "I'm a southern California native seeking to find work as a full stack web developer. I enjoying tinkering with computers and learning new things!"}}
 
-  describe "#scrape_index_page" do
+  describe ".scrape_index_page" do
     it "is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student" do
       index_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"
       scraped_students = Scraper.scrape_index_page(index_url)
@@ -32,7 +32,7 @@ describe "Scraper" do
     end
   end
 
-  describe "#scrape_profile_page" do
+  describe ".scrape_profile_page" do
     it "is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student" do
       profile_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/students/joe-burgess.html"
       scraped_student = Scraper.scrape_profile_page(profile_url)
